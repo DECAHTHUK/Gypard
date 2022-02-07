23bd00969f520b23092268cf56bd4264621c347d
Alexei Talanov <alexei.talanov003@mail.ru>
2022-02-08 01:02:49 +0300

    mvp

--- GypardC/Header.h
+++ GypardC/Header.h
@@ -1,1 +1,20 @@
 #pragma once
+
+DWORD WINAPI stick(LPVOID handle);
+
+DWORD WINAPI test(LPVOID shit);
+
+extern "C" __declspec(dllexport) void init_cons();
+
+void init_cons();
+
+void install_config(); //read conf from file
+
+void read_gyro(int handle);
+
+void button_inputs(int handle); // 5 per joycon
+
+void read_accelerometer(int handle);// for now idk what to do wit it
+
+#define DEFAULT_TICK 3
+#define WAITING_TICK 10
--- GypardC/Source.cpp
+++ GypardC/Source.cpp
@@ -1,12 +1,14 @@
 #define CRT_SECURE_NO_WARNINGS
-#include "JoyShockLibrary.h"
 #include <stdio.h>
 #define HAVE_STRUCT_TIMESPEC
 #include <conio.h>
 #include <math.h>
-#include <pthread.h>
 #include <stdlib.h>
 #include <windows.h>
+
+#include "JoyShockLibrary.h"
+
+#include "Header.h"
 
 int RightHandle;
 int LeftHandle;
@@ -14,24 +16,54 @@ JOY_SHOCK_STATE j1JSS, j2JSS;
 IMU_STATE j1IS, j2IS;
 MOTION_STATE j1MS, j2MS;
 
-extern "C" __declspec(dllexport) void Stick(int handle);
-void Stick(int handle) {
+void init_cons() {
+	int a = JslConnectDevices();
+	int *deviceHandleArray = (int *)malloc(a * sizeof(int));
+	JslGetConnectedDeviceHandles(deviceHandleArray, a);
+	RightHandle = deviceHandleArray[0];
+	j1JSS = JslGetSimpleState(deviceHandleArray[0]);
+	HANDLE htread, uh;
+	DWORD ThreadID, lelel;
+	htread = CreateThread(NULL, 0, stick, (void*)&deviceHandleArray[0], 0, &ThreadID);
+    uh = CreateThread(NULL, 0, test, NULL, 0, &lelel);
+}
+
+DWORD WINAPI test(LPVOID shit) {
+	while (true) {
+		j1JSS = JslGetSimpleState(0);
+		if (j1JSS.rTrigger == 1) {
+			POINT p;
+			GetCursorPos(&p);
+			mouse_event(MOUSEEVENTF_LEFTDOWN, p.x, p.y, 0, 0);
+			while (j1JSS.rTrigger == 1) {
+				j1JSS = JslGetSimpleState(0);
+                Sleep(WAITING_TICK);
+			}
+			GetCursorPos(&p);
+			mouse_event(MOUSEEVENTF_LEFTUP, p.x, p.y, 0, 0);
+		}
+		Sleep(WAITING_TICK);
+	}
+	return 0;
+}
+
+DWORD WINAPI stick(LPVOID handle) {
 	POINT p;
 	GetCursorPos(&p);
 	float StickX = p.x;
 	float StickY = p.y;
-	if (handle == LeftHandle) {
-		j1JSS = JslGetSimpleState(handle);
+	if (*((int *)handle) == RightHandle) {
+        j1JSS = JslGetSimpleState(*((int *)handle));
 		while (true) {
-			j1JSS = JslGetSimpleState(handle);
+            j1JSS = JslGetSimpleState(*((int *)handle));
 			if (j1JSS.stickRX != 0 || j1JSS.stickRY > 0.001) {
 				float temp = 5;
 				while (j1JSS.stickRX != 0 || j1JSS.stickRY > 0.001) {
-					j1JSS = JslGetSimpleState(handle);
+                    j1JSS = JslGetSimpleState(*((int *)handle));
 					StickX += j1JSS.stickRX * temp;
 					StickY -= (j1JSS.stickRY + 0.000488) * temp;
 					SetCursorPos(StickX, StickY);
-					Sleep(2);
+					Sleep(DEFAULT_TICK);
 					temp += 0.15;
 				}
 			} else {
@@ -39,17 +71,17 @@ void Stick(int handle) {
 				StickX = p.x;
 				StickY = p.y;
 			}
-			Sleep(2);
+			Sleep(DEFAULT_TICK);
 		}
+	}
+	return 0;
+	// TODO: leftCon
+}
+
+void button_inputs(int handle) {
+	if (handle == RightHandle) {
+		j1JSS = JslGetSimpleState(handle);
+        int btnId = JslGetButtons(handle);
 	}
 }
 
-extern "C" __declspec(dllexport) void InitCons();
-
-void InitCons() {
-  int a = JslConnectDevices();
-  int *deviceHandleArray = (int *)malloc(a * sizeof(int));
-  JslGetConnectedDeviceHandles(deviceHandleArray, a);
-  LeftHandle = deviceHandleArray[0];
-  Stick(deviceHandleArray[0]);
-}
